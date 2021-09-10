e303f43e0c344a438f7d7c5252d7f95968a1d2eb
Daniel Araújo <daniel.ti.info@gmail.com>
2021-09-10 18:00:43 -0300

    Configurações no Eureka Server

---
+++ service-discovery/src/test/java/com/github/dan/araujo/service/discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.dan.araujo.service.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
